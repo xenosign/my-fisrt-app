61e9e7a16ec653e0d53829a10cb75edaf9fae9ea
xenosign <xenosign@naver.com>
2022-10-07 02:41:47 +0900

    prettier

--- src/components/UsingUseMemo.js
+++ src/components/UsingUseMemo.js
@@ -1,27 +1,32 @@
-import { useEffect, useState, useMemo } from "react";
+import { useEffect, useState, useMemo } from 'react';
 
 export default function UsingUseMemo() {
-    const [number, setNumber] = useState(0);
-    const [isKorea, setIsKorea] = useState(true);
+  const [number, setNumber] = useState(0);
+  const [isKorea, setIsKorea] = useState(true);
 
-    const location = useMemo(() => {
-        return {
-            where: isKorea ? "한국" : "외국",
-        }
-    }, [isKorea]);
+  const location = useMemo(() => {
+    return {
+      where: isKorea ? '한국' : '외국',
+    };
+  }, [isKorea]);
 
-    useEffect(() => {
-        console.log("🐶 useEffect 호출");
-    }, [location]);
+  useEffect(() => {
+    console.log('🐶 useEffect 호출');
+  }, [location]);
 
-    return (
-        <>
-            <h1>숫자 증감</h1>
-            <input type="number" value={number} onChange={(e) => setNumber(parseInt(e.target.value))} />
-            <br /><br />
-            <h1>Where are you?</h1>
-            <h2>위치: {location.where}</h2>
-            <button onClick={() => setIsKorea(!isKorea)}>나라 변경</button>
-        </>
-    )
+  return (
+    <>
+      <h1>숫자 증감</h1>
+      <input
+        type="number"
+        value={number}
+        onChange={(e) => setNumber(parseInt(e.target.value))}
+      />
+      <br />
+      <br />
+      <h1>Where are you?</h1>
+      <h2>위치: {location.where}</h2>
+      <button onClick={() => setIsKorea(!isKorea)}>나라 변경</button>
+    </>
+  );
 }
